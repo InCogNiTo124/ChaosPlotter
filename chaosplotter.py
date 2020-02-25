baa1bbb79bce96e8de76be51761a9474b01d79d3
InCogNiTo124 <marijansmetko123@gmail.com>
2020-02-25 17:29:36 +0100

    Add bifurcation plotter

--- chaosplotter.py
+++ chaosplotter.py
@@ -1,50 +1,81 @@
-import kivy
+from kivy.config import Config
+Config.set('graphics', 'window_state', 'maximized')
+Config.set('graphics', 'fbo', 'hardware')
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ObjectProperty
 from kivy.garden.graph import Graph, LinePlot as Plot
+from kivy.graphics import Point, Color
 from kivy.garden.tickmarker import TickMarker
 from kivy.uix.slider import Slider
-#from kivy.uix.boxlayout import BoxLayout
+import itertools as it
+import concurrent.futures as cf
+import threading
 
-class TickSlider(Slider, TickMarker):
-    pass
+R_COUNT = 1000
+P_COUNT = 100
 
-ITER_COUNT = 5000
 def iterate(population, r):
     while True:
         yield population
         population = r * population * (1 - population)
     return
 
-def generate(population, r):
+def generate(population, r, maxiter):
     iterator = iterate(population, r)
-    sequence = [(i, next(iterator)) for i in range(ITER_COUNT)]
-    #print(sequence)
+    sequence = [(i, next(iterator)) for i in range(maxiter)]
     return sequence
+
+def get_last(p, r, iter_count):
+    iterator = iterate(p, r)
+    last = 0
+    for _ in range(iter_count):
+        last = next(iterator)
+    return last
+
+class TickSlider(Slider, TickMarker):
+    pass
 
 class ChaosPlotter(Widget):
     population = NumericProperty(0.8)
     r = NumericProperty(2)
     graph = ObjectProperty(None)
+    diagram = ObjectProperty(None)
     Graph() # There is a serious bug in a garden.graph library that can only be solved like this
     plot = Plot(color=(1, 0, 0, 1))
+    diag_size = (0, 0)
 
     def update(self):
         self.graph.remove_plot(self.plot)
-        #self.plot = Plot(color=(1, 0, 0, 1))
-        self.plot.points = generate(self.population, self.r)
+        self.plot.points = generate(self.population, self.r, self.graph.xmax)
         self.graph.add_plot(self.plot)
         return
     
     on_population = lambda self, a, b: self.update()
     on_r = on_population
 
+    def start_bifurcation(self, size):
+        print("SELF_DIAGSIZE", self.diag_size)
+        size_x, size_y = size
+        def f(self, size_x, size_y):
+            with cf.ThreadPoolExecutor(max_workers=5) as executor:
+                futures = {executor.submit(get_last, p/P_COUNT, 1 + 3*r/R_COUNT, self.graph.xmax): r/R_COUNT for p, r in it.product(range(1, P_COUNT), range(R_COUNT))}
+                for future in cf.as_completed(futures):
+                    r = futures[future]
+                    p = future.result()
+                    with self.diagram.canvas:
+                        Color(0, 0, 0)
+                        Point(points=[r*size_x, p*size_y], pointsize=0.5)
+
+            return
+        threading.Thread(target=f, args=(self,size_x, size_y)).start()
+        return
+
 class ChaosPlotterApp(App):
     def build(self):
-        plotter = ChaosPlotter()
-        plotter.update()
-        return plotter
+        self.plotter = ChaosPlotter()
+        return self.plotter
 
 if __name__ == "__main__":
-    ChaosPlotterApp().run()
+    a = ChaosPlotterApp()
+    a.run()
