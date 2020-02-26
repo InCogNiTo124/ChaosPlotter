c443268a1cdaef3ed1d75ec0edba62b5b1758bc0
InCogNiTo124 <marijansmetko123@gmail.com>
2020-02-26 19:04:20 +0100

    Add selection of different iterating functions

--- chaosplotter.py
+++ chaosplotter.py
@@ -16,6 +16,7 @@ from kivy.uix.slider import Slider
 import itertools as it
 import concurrent.futures as cf
 import threading
+import math
 
 R_COUNT = 1000
 P_COUNT = 100
@@ -31,40 +32,44 @@ def generate(population, r, maxiter):
     sequence = [(i, next(iterator)) for i in range(maxiter)]
     return sequence
 
-def get_last(p, r, iter_count):
-    iterator = iterate(p, r)
-    last = 0
+def get_last(function, r, p, iter_count):
+    last = p
     for _ in range(iter_count):
-        last = next(iterator)
+        last = function(r, last)
     return last
 
 class TickSlider(Slider, TickMarker):
     pass
 
 class ChaosPlotter(Widget):
+    FUNCTIONS = [
+        lambda r, p: r * p * (1 - p),
+        lambda r, p: r/4 * math.sin(math.pi * p)]
     population = NumericProperty(0.8)
     r = NumericProperty(2)
     graph = ObjectProperty(None)
     diagram = ObjectProperty(None)
+    function_index = NumericProperty(None)
     Graph() # There is a serious bug in a garden.graph library that can only be solved like this
     plot = Plot(color=(1, 0, 0, 1))
     diag_size = (0, 0)
 
-    def update(self):
+    on_function_index = lambda self, a, b: self.start_bifurcation(self.diag_size, self.FUNCTIONS[self.function_index])
+
+    def update_graph(self):
         self.graph.remove_plot(self.plot)
         self.plot.points = generate(self.population, self.r, self.graph.xmax)
         self.graph.add_plot(self.plot)
         return
     
-    on_population = lambda self, a, b: self.update()
+    on_population = lambda self, a, b: self.update_graph()
     on_r = on_population
 
-    def start_bifurcation(self, size):
-        print("SELF_DIAGSIZE", self.diag_size)
+    def start_bifurcation(self, size, function):
         size_x, size_y = size
         def f(self, size_x, size_y):
             with cf.ThreadPoolExecutor(max_workers=5) as executor:
-                futures = {executor.submit(get_last, p/P_COUNT, 1 + 3*r/R_COUNT, self.graph.xmax): r/R_COUNT for p, r in it.product(range(1, P_COUNT), range(R_COUNT))}
+                futures = {executor.submit(get_last, function, 1 + 3*r/R_COUNT, p/P_COUNT, self.graph.xmax): r/R_COUNT for p, r in it.product(range(1, P_COUNT), range(R_COUNT))}
                 for future in cf.as_completed(futures):
                     r = futures[future]
                     p = future.result()
