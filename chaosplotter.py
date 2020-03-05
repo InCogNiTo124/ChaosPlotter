82fb7741f4c6f76304c06425ee70d2b62db82b76
InCogNiTo124 <marijansmetko123@gmail.com>
2020-03-05 12:23:56 +0100

    Switch to Qt5 due to Kivi's slowness
    Kivy proved itself to be very clumsy and slow, and although it's got a
    great language for defining a UI like CSS, it's performace is subpar.
    Qt OTOH is faster, and more importantly has "painless" (not really)
    integration with matplotlib, which implies numpy, therefore speeding
    thing up considerably and significantly.

--- chaosplotter.py
+++ chaosplotter.py
@@ -1,162 +1,19 @@
-from kivy.config import Config
-Config.set('graphics', 'resizable', False)
-Config.set('graphics', 'fbo', 'hardware')
-Config.set('graphics', 'width', '1280')
-Config.set('graphics', 'height', '960')
-Config.set('graphics', 'borderless', False)
+import sys
+import numpy as np
+from PyQt5.QtWidgets import QMainWindow, QWidget, QApplication
+from ui import createUI
 
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ObjectProperty
-from kivy.garden.graph import Graph, LinePlot as Plot
-from kivy.graphics import Point, Color, Line
-from kivy.graphics.vertex_instructions import GraphicException
-from kivy.garden.tickmarker import TickMarker
-from kivy.uix.slider import Slider
-import itertools as it
-import concurrent.futures as cf
-import threading
-import math
-
-R_COUNT = 1000
-P_COUNT = 100
-
-def iterate(f, r, population):
-    while True:
-        yield population
-        population = f(r, population)
-    return
-
-def generate(fun, r, population, maxiter):
-    iterator = iterate(fun, r, population)
-    sequence = [(i, next(iterator)) for i in range(maxiter)]
-    del(iterator)
-    return sequence
-
-def get_last(function, r_factor, population, iter_count):
-    iterator = iterate(function, r_factor, population)
-    last = next(iterator)
-    for _ in range(iter_count):
-        last = next(iterator)
-    return last
-
-class TickSlider(Slider, TickMarker):
-    pass
-
-class ChaosPlotter(Widget):
-    FUNCTIONS = [
-        lambda r, p: r * p * (1 - p),
-        lambda r, p: r * math.sin(math.pi * p),
-        lambda r, p: r * (1 - math.cos(2*math.pi*p)) / 2,
-        lambda r, p: r * p*p*(1-p),
-        lambda r, p: r * p*(1 - p*p)]
-    population = NumericProperty(0.74)
-    r = NumericProperty(3.2)
-    r_slider = ObjectProperty(None)
-    graph = ObjectProperty(None)
-    diagram = ObjectProperty(None)
-    progress = ObjectProperty(None)
-    function_index = NumericProperty(None)
-    Graph() # There is a serious bug in a garden.graph library that can only be solved like this
-    plot = Plot(color=(1, 0, 0, 1))
-    diag_size = (0, 0)
-    indicator = Line(points=[], width=1.0)
-
-    def update_r_slider(self):
-        n = self.r_slider.value_normalized
-        if self.function_index == 0:
-            self.r_slider.min = 1
-            self.r_slider.max = 4
-        elif self.function_index == 1:
-            self.r_slider.min = 0.5
-            self.r_slider.max = 1
-        elif self.function_index == 2:
-            self.r_slider.min = 0.439
-            self.r_slider.max = 0.879
-        elif self.function_index == 3:
-            self.r_slider.min = 4
-            self.r_slider.max = 6.543
-        elif self.function_index == 4:
-            self.r_slider.min = 1
-            self.r_slider.max = 3
-        self.r_slider.value_normalized = n
+class ChaosPlotter(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.main = QWidget()
+        self.setCentralWidget(self.main)
+        self.main.setLayout(createUI(self))
         return
-
-    def on_function_index(self, a, b):
-        self.update_r_slider()
-        self.start_bifurcation(self.diag_size, self.FUNCTIONS[self.function_index])
-        return
-
-    def update_indicator(self):
-        if self.function_index is not None:
-            self.diagram.canvas.remove(self.indicator)
-            start_x, start_y = self.diagram.pos
-            self.indicator.points = [
-                start_x + self.r_slider.value_normalized * self.diagram.width,
-                start_y + self.diagram.height,
-                start_x + self.r_slider.value_normalized * self.diagram.width,
-                start_y
-            ]
-            with self.diagram.canvas:
-                Color(1, 0, 0)
-            self.diagram.canvas.add(self.indicator)
-            with self.diagram.canvas:
-                Color(0, 0, 0)
-            self.diagram.canvas.add(self.indicator)
-        return
-
-    def update_graph(self):
-        if self.function_index is not None:
-            self.graph.remove_plot(self.plot)
-            self.plot.points = generate(self.FUNCTIONS[self.function_index], self.r, self.population, self.graph.xmax)
-            self.graph.add_plot(self.plot)
-        return
-    
-    on_population = lambda self, a, b: self.update_graph()
-
-    def on_r(self, a, b):
-        self.update_indicator()
-        self.update_graph()
-        return
-
-    def start_bifurcation(self, size, function):
-        self.progress.value = 0
-        self.update_graph()
-        self.diagram.canvas.clear()
-        size_x, size_y = size
-        def f(self, size_x, size_y):
-            r_min = self.r_slider.min
-            r_max = self.r_slider.max
-            with cf.ThreadPoolExecutor(max_workers=5) as executor:
-                futures = {
-                    executor.submit(get_last, function, r_min + (r_max - r_min)*r/R_COUNT, p/P_COUNT, self.graph.xmax): r/R_COUNT
-                    for p, r in it.product(range(1, P_COUNT), range(R_COUNT))
-                }
-                self.progress.max = len(futures)
-                points = Point(points=[], pointsize=0.5)
-                with self.diagram.canvas:
-                    Color(0, 0, 0)
-                self.diagram.canvas.add(points)
-                for future in cf.as_completed(futures):
-                    self.progress.value += 1
-                    r = futures[future]
-                    p = future.result()
-                    try:
-                        points.add_point(r*size_x, p*size_y)
-                    except GraphicException:
-                        points = Point(points=[r*size_x, p*size_y], pointsize=0.5)
-                        self.diagram.canvas.add(points)
-
-            return
-        threading.Thread(target=f, args=(self,size_x, size_y)).start()
-        return
-
-class ChaosPlotterApp(App):
-    def build(self):
-        self.plotter = ChaosPlotter()
-        return self.plotter
 
 if __name__ == "__main__":
-    a = ChaosPlotterApp()
-    a.run()
+    qapp = QApplication(sys.argv)
+    plotter = ChaosPlotter()
+    plotter.show()
+    qapp.exec()
 
---
+++ ui.py
@@ -0,0 +1,65 @@
+import numpy as np
+from PyQt5.QtCore import Qt
+from matplotlib.backends.backend_qt5agg import FigureCanvas, NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+from PyQt5.QtWidgets import (
+    QWidget, QComboBox, QHBoxLayout, QVBoxLayout, QSpacerItem, QSizePolicy, QSlider, QLabel
+)
+
+class Graph(FigureCanvas):
+    def __init__(self, figsize):
+        fig = Figure(figsize=figsize)
+        self.axes = fig.add_subplot(1, 1, 1)
+        super().__init__(fig)
+        return
+
+    def plot(self, *args, **kwargs):
+        return self.axes.plot(*args, **kwargs)
+
+def populateComboBoxes(self):
+    box_cb = QHBoxLayout()
+    functions_cb = QComboBox()
+    for item in ["R \u00b7 P\u2099(1 - P\u2099)", "R \u00b7 sin(P\u2099)"]:
+        functions_cb.addItem(item)
+    box_cb.addWidget(functions_cb)
+    box_cb.addItem(QSpacerItem(10, 10, QSizePolicy.Expanding))
+    graph_cb = QComboBox()
+    for item in ["P\u2099", "\u2131[P\u2099]", "P\u2099\u208A\u2081 - P\u2099", "\u2131[P\u2099\u208A\u2081 - P\u2099]"]:
+        graph_cb.addItem(item)
+    box_cb.addWidget(graph_cb)
+    return box_cb
+
+def populateSliderGraph(self):
+    box_sg = QHBoxLayout()
+    self.population_slider = QSlider(Qt.Vertical)
+    self.population_slider.setTickPosition(QSlider.TicksBothSides)
+    box_sg.addWidget(self.population_slider)
+    self.graph = Graph(figsize=(10, 3))
+    box_sg.addWidget(self.graph)
+    return box_sg
+
+def populateLabels(self):
+    box_labels = QHBoxLayout()
+    self.population_label = QLabel("R = {}")
+    self.r_label = QLabel("P = {}")
+    box_labels.addWidget(self.population_label)
+    box_labels.addWidget(self.r_label)
+    box_labels.addItem(QSpacerItem(10, 10, QSizePolicy.Expanding))
+    return box_labels
+
+def createUI(self):
+    box_v = QVBoxLayout(self.main) 
+    box_v.addLayout(populateComboBoxes(self))
+    box_v.addLayout(populateSliderGraph(self))
+    box_v.addLayout(populateLabels(self))
+    self.R_slider = QSlider(Qt.Horizontal)
+    box_v.addWidget(self.R_slider)
+
+    self.plot = Graph(figsize=(8, 5))
+    box_v.addWidget(self.plot)
+    x = np.linspace(-5, 5, 101)
+    y = np.exp(-x**2)
+    self.graph.plot(x, y)
+
+    self.plot.plot(np.linspace(-10, 10, 501), np.random.randn(501))
+    return box_v
